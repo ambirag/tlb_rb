9fde90916c9a6cedaf601458b25d4a42c50a59eb
Janmejay Singh <singh.janmejay@gmail.com>
2010-12-04 07:29:49 +0530

    task is defined in initialize, no point adding to libs after that

--- lib/tlb/test_unit/test_task.rb
+++ lib/tlb/test_unit/test_task.rb
@@ -3,8 +3,10 @@ require 'rake/testtask'
 
 class Tlb::TestUnit::TestTask < Rake::TestTask
   def initialize *args
-    super
-    @libs << File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
-    @ruby_opts.unshift " -r#{File.join('tlb', 'test_unit', 'mediator_inflection.rb')} "
+    super do |this|
+      this.libs << File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
+      this.ruby_opts.unshift " -r#{File.join('tlb', 'test_unit', 'mediator_inflection.rb')} "
+      yield this if block_given?
+    end
   end
 end

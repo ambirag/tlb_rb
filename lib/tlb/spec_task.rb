03f285c3e544ccf6d3e6aaac3523d1031deb9b18
Janmejay Singh <singh.janmejay@gmail.com>
2010-12-06 22:00:16 +0530

    enhanced spec_task to hook-up spec_formatter internally

--- lib/tlb/spec_task.rb
+++ lib/tlb/spec_task.rb
@@ -1,6 +1,22 @@
-require File.join(File.dirname(__FILE__), 'spec_task_enhancement')
 require 'spec/rake/spectask'
 
 class Tlb::SpecTask < Spec::Rake::SpecTask
-  include Tlb::SpecTaskEnhancement
+  attr_accessor :tlb_out
+
+  def initialize *args
+    path_to_tlb = File.expand_path(File.join(File.dirname(__FILE__), '..', 'tlb'))
+    path_to_spec_formatter = File.expand_path(File.join(File.dirname(__FILE__), 'spec_formatter'))
+    self.tlb_out = '/dev/null'
+    super do |this|
+      yield this if block_given?
+      this.spec_opts.unshift "--require #{path_to_tlb},#{path_to_spec_formatter} --format 'Tlb::SpecFormatter:#{this.tlb_out}'"
+    end
+  end
+
+  alias_method :rspec_spec_file_list, :spec_file_list
+
+  def spec_file_list
+    balanced_and_reordered = Tlb.balance_and_order(rspec_spec_file_list.to_a)
+    FileList[*balanced_and_reordered]
+  end
 end
--- spec/tlb/spec_task_spec.rb
+++ spec/tlb/spec_task_spec.rb
@@ -2,6 +2,10 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 require 'spec_task'
 
 describe Tlb::SpecTask do
+  before(:all) do
+    @path_to_tlb = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'tlb'))
+    @path_to_spec_formatter = File.expand_path(File.join(@path_to_tlb, 'spec_formatter'))
+  end
 
   it "should return balanced and ordered subset" do
     @task = Tlb::SpecTask.new
@@ -11,5 +15,26 @@ describe Tlb::SpecTask do
     balanced_list = @task.spec_file_list
     balanced_list.should be_a(Rake::FileList)
     balanced_list.to_a.should == ['quux.rb', 'foo.rb']
+  end
+
+  it "should hookup formatter so feedback is posted" do
+    @task = Tlb::SpecTask.new
+    @task.spec_opts.should == ["--require #{@path_to_tlb},#{@path_to_spec_formatter} --format 'Tlb::SpecFormatter:/dev/null'"]
+  end
+
+  it "should honor user specified attributes" do
+    @task = Tlb::SpecTask.new(:foo) do |t|
+      t.spec_opts << "--require foo_bar"
+      t.spec_opts << "--require baz_quux"
+    end
+    @task.spec_opts.should == ["--require #{@path_to_tlb},#{@path_to_spec_formatter} --format 'Tlb::SpecFormatter:/dev/null'", "--require foo_bar", "--require baz_quux"]
+    @task.name.should == :foo
+  end
+
+  it "should use specified output file for tlb's spec_formatter" do
+    @task = Tlb::SpecTask.new(:foo) do |t|
+      t.tlb_out = "/tmp/tlb_spec_formatter_out"
+    end
+    @task.spec_opts.should == ["--require #{@path_to_tlb},#{@path_to_spec_formatter} --format 'Tlb::SpecFormatter:/tmp/tlb_spec_formatter_out'"]
   end
 end

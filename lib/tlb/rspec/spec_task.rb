239872ff188667c31aa529a993fda48859bc54fb
Janmejay Singh <singh.janmejay@gmail.com>
2011-02-10 06:11:35 +0530

    issue#57 - fixed tlb.rb being loaded multiple times problem

--- lib/tlb/rspec/spec_task.rb
+++ lib/tlb/rspec/spec_task.rb
@@ -3,11 +3,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tlb'))
 
 class Tlb::RSpec::SpecTask < RSpec::Core::RakeTask
   def initialize *args
-    path_to_tlb = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tlb'))
     super do |this|
       yield this if block_given?
       this.rspec_opts ||= ''
-      this.rspec_opts = " --require #{path_to_tlb} --require tlb/rspec/reporter_inflection " + this.rspec_opts
+      this.rspec_opts = " --require tlb/rspec/reporter_inflection " + this.rspec_opts
     end
   end
 
--- lib/tlb/test_unit/test_task.rb
+++ lib/tlb/test_unit/test_task.rb
@@ -5,8 +5,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tlb'))
 class Tlb::TestUnit::TestTask < Rake::TestTask
   def initialize *args
     super do |this|
-      this.ruby_opts.unshift " -r#{File.join('tlb', 'test_unit', 'mediator_inflection')} "
-      this.ruby_opts.unshift " -r#{File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tlb'))} "
+      this.ruby_opts.unshift " -r#{File.expand_path(File.join(File.dirname(__FILE__), 'mediator_inflection'))} "
       yield this if block_given?
     end
   end

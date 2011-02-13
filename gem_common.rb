9c210ffdd697dc020ae9cfb2e0f81e32b90f5512
Janmejay Singh <singh.janmejay@gmail.com>
2011-02-13 09:08:59 +0530

    updated text: points to the new website + gem post install notice discusses examples in slightly more details, and refers to download page

--- gem_common.rb
+++ gem_common.rb
@@ -5,21 +5,29 @@ TAG_VERSION = `git describe --abbrev=0`.gsub(/^v/, '').gsub(/-rspec-1$/, '')
 CODE_VERSION = `git describe --always`
 AUTHORS = ["Janmejay Singh", "Pavan KS"]
 EMAIL = "singh.janmejay@gmail.com;itspanzi@gmail.com"
-HOME_PAGE = "http://github.com/test-load-balancer/tlb_rb"
+HOME_PAGE = "http://github.com/test-load-balancer/tlb.rb"
 SUMMARY = "#{$name}-#{CODE_VERSION}"
 DESC = <<END
 TLB ruby implementation base, which provides support for load balancing tests written in #{$framework}.
-TLB_rb test suite is not bundled, please check http://github.com/test-load-balancer/tlb_rb for tests.
-Detailed configuration documentation can be found at https://github.com/test-load-balancer/tlb/wiki.
+TLB.rb test suite is not bundled, please check http://github.com/test-load-balancer/tlb.rb for tests.
+Detailed documentation is available at http://test-load-balancer.github.com.
 END
 POST_INSTALL_MESSAGE = <<END
 -------------------------------------------------------------------------
-Documentation: Detailed configuration documentation can be found at https://github.com/test-load-balancer/tlb/wiki.
------------------------------
-Example: https://github.com/test-load-balancer/sample_projects has examples projects and shell script to demonstrate a typical build(by starting a local tlb server, and executing two partitions locally). While partitions in these examples execute one after another, in an actual CI/pre-checkin build, they will actually run parallely on different machines.
-Its a good idea to play with the environment variables values being used in these shell-scripts to understand how they affect TLB's behaviour. You may want to check https://github.com/test-load-balancer/tlb/wiki/Configuration-Variables, which documents each variable and its effect.
------------------------------
-Issue tracker: Please report bugs/enhancements/feature-requests at http://code.google.com/p/tlb/issues/list. Github, Rubyforge or any other issue trackers are not monitored or updated.
+TLB Documentation: Detailed configuration documentation can be found at http://test-load-balancer.github.com. Documentation section in this website hosts documentation for every public release.
+
+-------------------------------------------------------------------------
+TLB Example(s): We maintain a directory of tlb-enabled dummy projects written in different languages using different testing and build frameworks to help new TLB users get started and provide people a working project to refer to while hooking up TLB on their project(s). Each of these projects have a shell script(named run_balanced.sh) that is meant to demonstrate a typical tlb-enabled build(by starting a local tlb server, and executing two partitions that run dummy tests locally).
+For demonstration purpose, aforementioned shell script executes partitions in the example-project one after another(serially). However, partitions will be executed parallely on different machines in a real-world setup(hence cutting the build time).
+We recomend playing with the configuration-variable values being set in the shell-script(s) to understand the effect different values have on load-balancing/reordering behaviour. You may want to check http://test-load-balancer.github.com, which links to 'detailed documentation' that covers each configuration variable and explains its purpose, effect and implecation.
+
+Examples archive is released alongwith TLB, and is available for download at http://code.google.com/p/tlb/downloads/list.
+
+To execute the example project, drop into the example project directory(examples/rspec2_example for instance) and invoke the './run_balanced.sh'.
+
+-------------------------------------------------------------------------
+TLB Issue Tracker: Please report/port bugs/enhancements/feature-requests on http://code.google.com/p/tlb/issues/list. Github, Rubyforge or any other issue trackers are not monitored or updated.
+
 -------------------------------------------------------------------------
 END
 RUBYFORGE_PROJECT = "tlb-rb"

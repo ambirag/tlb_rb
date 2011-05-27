e23fc049b82498d49bae31efb66971e277759506
Janmejay Singh <singh.janmejay@gmail.com>
2011-05-27 15:54:45 +0530

    old jruby had some bug that was hit while installing rspec-1, so using latest jruby, which has it fixed

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ run_command_with ruby-1.8.7-p334@tlb rake test
 
 run_command_with ruby-1.9.2-head@tlb rake test
 
-run_command_with jruby-1.6.0@tlb rake test
+run_command_with jruby-head@tlb rake test
 
 

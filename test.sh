c096f447e1775e72487e5a1c9d40d7bd4e6c3206
Janmejay Singh <singh.janmejay@gmail.com>
2011-12-09 07:05:18 +0530

    test.sh now checks for a variable and declaratively disables running on tests on 1.8.7(MRI)

--- test.sh
+++ test.sh
@@ -2,7 +2,11 @@
 
 source execution_helper.sh
 
-run_command_with ruby-1.8.7-head@tlb rake test
+ignore_187=${ignore_187:-false}
+
+if [ $ignore_187 != 'true' ]; then
+    run_command_with ruby-1.8.7-head@tlb rake test
+fi
 
 run_command_with ruby-1.9.2-head@tlb rake test
 
